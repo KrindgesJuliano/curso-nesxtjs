e7c19863947af92b6e756923930163bee3d6fa09
JulianoKrindges <juliano9krindges@hotmail.com>
2020-10-01 22:45:48 -0400

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import GlobalStyle from '../../styles/globalStyles'
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
